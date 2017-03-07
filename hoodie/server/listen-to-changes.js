c39e0d48d103e011d6a8e3732eb13400dbb82139
Gregor <gregor@martynus.net>
2017-03-07 07:33:53 -0800

    fix: create user db if not existent

--- hoodie/server/listen-to-changes.js
+++ hoodie/server/listen-to-changes.js
@@ -7,8 +7,28 @@ function listenToChanges (server, name) {
 
   return Store.open(name)
 
+  .catch((error) => {
+    server.log(['warn', 'listen-to-changes'], `Database missing for ${name}`)
+
+    if (error.status === 404) {
+      const idRole = 'id:' + name.substr('user/'.length)
+      return Store.create(name, {
+        access: ['read', 'write'],
+        role: [idRole]
+      })
+
+      .then(function (name) {
+        server.log(['info', 'listen-to-changes'], `Database ${name} created with read/write access for ${idRole}`)
+
+        return Store.open(name)
+      })
+    }
+
+    throw error
+  })
+
   .then((store) => {
-    server.log(['verbose', 'change'], `Listening to changes in ${name}`)
+    server.log(['verbose', 'listen-to-changes'], `Listening to changes in ${name}`)
 
     // once https://github.com/hoodiehq/pouchdb-hoodie-api/pull/123 is merged we
     // can use store.on('change'). Until then we have to use PouchDB’s changes
