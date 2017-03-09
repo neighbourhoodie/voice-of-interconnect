29d4d5d1ea15e4d27cc3e620bf0369cb4e4202e5
Steven Trevathan <stevebot@Stevebot.local>
2017-03-09 00:37:15 -0500

    progress on hooking up recording cards to actual data

--- app/lib/notes-list.js
+++ app/lib/notes-list.js
@@ -2,7 +2,7 @@
 module.exports = notesList
 
 function notesList (hoodie) {
-  var $notes = document.querySelector('#notes')
+  var $notes = document.querySelector('#recordings')
 
   hoodie.store.on('change', render.bind(null, $notes, hoodie))
   render($notes, hoodie)
@@ -19,7 +19,28 @@ function render ($notes, hoodie) {
         return !!doc.progress
       })
       .map(function (doc) {
-        return '<tr data-id="' + doc.id + '"><td>' + doc.id + '</td><td><button data-action="play">play</button></td><td><pre>' + JSON.stringify(doc, null, 2) + '</pre></td></tr>'
+
+        if (doc.progress.length > 0) {
+          var docProgress = doc.progress[doc.progress.length - 1].type
+        }
+
+        if (docProgress == "transcription") {
+          var statusText = "Processing"
+          var statusDesc = "Converting audio to text..."
+        } else if (docProgress == "analysis") {
+          var statusText = "Analyzing"
+          var statusDesc = "Detecting sentiment..."
+        }
+
+        var docText = doc.text
+
+        if (doc.sentiment) {
+          var sentimentClass = doc.sentiment > 0 ? 'happy' : 'sad';
+
+          return '<li data-id="' + doc.id + '" class="analyzed"><svg width="19px" height="19px" viewBox="0 0 19 19"><use xlink:href="#face-' + sentimentClass + '"></use></svg><p class="depiction analyzed__'+ sentimentClass +'">' + doc.text + '</p></li><span class="hidden-reference"><button data-action="play">play</button></td><td><pre>' + JSON.stringify(doc, null, 2) + '</pre></span>'
+        } else {
+          return '<li data-id="' + doc.id + '"><span class="progress"><span class="progress_bar"></span></span><p class="depiction">' + doc.text + '</p><span class="status' + doc.progress + '"><strong>' + statusText + '</strong> &mdash; ' + statusDesc + '</span></li><span class="hidden-reference"><button data-action="play">play</button></td><td><pre>' + JSON.stringify(doc, null, 2) + '</pre></span>'
+        }
       }).join('\n')
 
     $notes.innerHTML = html
