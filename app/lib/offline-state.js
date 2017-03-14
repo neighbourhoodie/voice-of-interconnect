79358aff8ca818c67d94a4a8a0f2b5c6f64713b9
Steven Trevathan <stevebot@Stevebot.fios-router.home>
2017-03-13 23:05:14 -0400

    linted

--- app/lib/offline-state.js
+++ app/lib/offline-state.js
@@ -5,32 +5,28 @@ const $systemStateOnline = document.querySelector('#restored')
 const $recordings = document.querySelector('#stage__recordings')
 
 function detectOffline (hoodie) {
-
   hoodie.connectionStatus.startChecking({interval: 3000}).then(function () {
-
-    function handleOfflineState() {
+    function handleOfflineState () {
       $recordings.classList.add('offline')
       $systemStateOffline.classList.add('offline')
     }
 
-    function handleOnlineState() {
+    function handleOnlineState () {
       $recordings.classList.remove('offline')
       $systemStateOffline.classList.remove('offline')
 
       $systemStateOnline.classList.add('online')
-      setTimeout(function(){
+      setTimeout(function () {
         $systemStateOnline.classList.remove('online')
       }, 3000)
     }
 
     if (hoodie.connectionStatus.ok === false) {
       // we are offline
-      handleOfflineState();
+      handleOfflineState()
     }
 
     hoodie.connectionStatus.on('disconnect', handleOfflineState)
     hoodie.connectionStatus.on('reconnect', handleOnlineState)
-
   })
-
 }
