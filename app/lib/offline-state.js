f013033a8b6542f3049046a22f820eb1b6bc833d
Gregor Martynus <gregor@martynus.net>
2017-03-13 21:38:43 -0700

    subscribe to disconnect / reconnect events before `startChecking` resolves

--- app/lib/offline-state.js
+++ app/lib/offline-state.js
@@ -5,28 +5,28 @@ const $systemStateOnline = document.querySelector('#restored')
 const $recordings = document.querySelector('#stage__recordings')
 
 function detectOffline (hoodie) {
+  hoodie.connectionStatus.on('disconnect', handleOfflineState)
+  hoodie.connectionStatus.on('reconnect', handleOnlineState)
+
   hoodie.connectionStatus.startChecking({interval: 3000}).then(function () {
-    function handleOfflineState () {
-      $recordings.classList.add('offline')
-      $systemStateOffline.classList.add('offline')
-    }
-
-    function handleOnlineState () {
-      $recordings.classList.remove('offline')
-      $systemStateOffline.classList.remove('offline')
-
-      $systemStateOnline.classList.add('online')
-      setTimeout(function () {
-        $systemStateOnline.classList.remove('online')
-      }, 3000)
-    }
-
     if (hoodie.connectionStatus.ok === false) {
       // we are offline
       handleOfflineState()
     }
-
-    hoodie.connectionStatus.on('disconnect', handleOfflineState)
-    hoodie.connectionStatus.on('reconnect', handleOnlineState)
   })
+}
+
+function handleOfflineState () {
+  $recordings.classList.add('offline')
+  $systemStateOffline.classList.add('offline')
+}
+
+function handleOnlineState () {
+  $recordings.classList.remove('offline')
+  $systemStateOffline.classList.remove('offline')
+
+  $systemStateOnline.classList.add('online')
+  setTimeout(function () {
+    $systemStateOnline.classList.remove('online')
+  }, 3000)
 }
