9d43295c09e18703a61ab6939e0785fbd3a3b00b
Gregor <gregor@martynus.net>
2017-03-15 10:36:43 -0700

    feat: prevent redirect with `NO_REDIRECT=1`

--- hoodie/server/redirect.js
+++ hoodie/server/redirect.js
@@ -11,13 +11,21 @@ function redirect (server, options, next) {
     const host = request.headers['x-forwarded-host'] ? request.headers['x-forwarded-host'] : request.headers.host
     const isLocal = /^(localhost|127\.0\.0\.1)/.test(host)
 
-    if (isHttp && !isLocal) {
-      return reply()
-        .redirect('https://' + host + request.url.path)
-        .code(301)
+    if (process.env.NO_REDIRECT) {
+      return reply.continue()
     }
 
-    reply.continue()
+    if (isLocal) {
+      return reply.continue()
+    }
+
+    if (!isHttp) {
+      return reply.continue()
+    }
+
+    return reply()
+      .redirect('https://' + host + request.url.path)
+      .code(301)
   })
   next()
 }
